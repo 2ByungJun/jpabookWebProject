0b5a413f7aa51dab03617390f20d1e8cc34b092b
2ByungJun <ieed0205@naver.com>
2021-10-10 13:20:59 +0900

    회원 Repository 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository // 자동으로 Spring bean 에 자동으로 등록됨
+public class MemberRepository {
+
+    @PersistenceContext // JPA 엔티티 매니저를 자동으로 주입해 줌
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    // 이름에 의해 조회
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
